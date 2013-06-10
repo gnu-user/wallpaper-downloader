2d2e5273e058363f77e9b16f93932aefb2cd0d52
gnu_user <gsoc.student@gmail.com>
2013-06-10 01:12:25 -0400

    Added the REST api for the website
    The REST api at the moment provides basic support for getting the
    current progress of a request and when the files are ready the download
    url.

---
+++ api/index.php
@@ -0,0 +1,78 @@
+<?php 
+/*
+ * CS-CLUB Elections Website
+*
+* Copyright (C) 2013 Jonathan Gillett, Joseph Heron, Computer Science Club at DC and UOIT
+* All rights reserved.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+require 'Slim/Slim.php';
+\Slim\Slim::registerAutoloader();
+
+
+/*
+ * The REST web service api which provides access to get the progress of jobs
+ * and the final compressed zip file of images when they are ready for download
+ */
+$app = new \Slim\Slim();
+
+/* Gets the progress of a download request submitted by a user */
+$app->get('/progress/:session_id', 'getProgress');
+$app->get('/download/:session_id', 'getDownload');
+
+$app->run();
+
+
+/** 
+ * Get the progress of a download request submitted by the user
+ * @package api
+ *
+ * @param string $session_id The session id of the user who submitted the request
+ */
+function getProgress($session_id)
+{
+	global $db_user, $db_pass, $db_elec_name;
+
+	/* Connect to Redis */
+	$redis = new Redis();
+	$redis->connect('127.0.0.1');
+
+	/* check connection */
+
+	/* Get the progress of the job */
+	echo json_encode($redis->get($session_id));
+}
+
+/** 
+ * Get the location of the file to download for the request submitted by the user
+ * @package api
+ *
+ * @param string $session_id The session id of the user who submitted the request
+ */
+function getDownload($session_id)
+{
+	global $db_user, $db_pass, $db_elec_name;
+
+	/* Connect to Redis */
+	$redis = new Redis();
+	$redis->connect('127.0.0.1');
+
+	/* check connection */
+
+	/* Get the progress of the job */
+	echo json_encode($redis->get($session_id . ':download'));
+}
+
+?>
